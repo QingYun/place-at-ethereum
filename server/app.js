846582eda98afe189c2cd3e2ccae470f86081fdd
Jun Sun <qingyun95@gmail.com>
2017-10-05 17:18:54 +1030

    auto unlock account using env variables

--- server/app.js
+++ server/app.js
@@ -1,17 +1,15 @@
 const Web3 = require('web3')
 const R = require('ramda');
-const web3 = new Web3(new Web3.providers.WebsocketProvider('ws://127.0.0.1:8546'));
-const contracts = require('./contracts')(web3);
+const provider = new Web3.providers.WebsocketProvider('ws://127.0.0.1:8546')
+const web3 = new Web3(provider);
 
-R.chain(R.toPairs, R.values(contracts).map(c => c.events))
-  .map(([n, e]) => {
-    const log = (t) => console.log(n, t)
-    //e().on('data', log).on('changed', log).on('error', log)
-  })
-
-const run = () => 
+provider.on('connect', () => {
+  let account = null;
   web3.eth.getAccounts().then(accounts => {
-    const account = accounts[0]
+    account = accounts[process.env.ACC_INDEX]
+    return web3.eth.personal.unlockAccount(account, process.env.ACC_PASS, 0);
+  }).then(() => {
+    const contracts = require('./contracts')(web3);
 
     contracts
       .Gateway.methods.draw(1, 1, 1, web3.utils.padLeft('0x123', 32)).send({ from: account })
@@ -20,5 +18,4 @@ const run = () =>
       .then(console.log)
       .catch(console.log)
   })
-
-setTimeout(run, 200)
+});
